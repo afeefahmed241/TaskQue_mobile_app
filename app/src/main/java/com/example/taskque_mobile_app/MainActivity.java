38eed4c6086c0fa1387c6b74e15ec303681be8a5
Intiser <chowdhuryintiser19@gmail.com>
2021-07-15 13:57:34 +0600

    changed home screen, added fragment and buttons.

--- app/src/main/java/com/example/taskque_mobile_app/MainActivity.java
+++ app/src/main/java/com/example/taskque_mobile_app/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.taskque_mobile_app;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,9 +24,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button pendingBtn,overdueBtn;
+        pendingBtn=findViewById(R.id.pending_button);
+        overdueBtn=findViewById(R.id.overdue_button);
+
+        setFragment(new PedingFragment());
+
+
+        pendingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+                replaceFragment(new PedingFragment());
+                    
+                    
+                
+
+            }
+        });
+
+        overdueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                replaceFragment(new OverDueFragment());
+
+
+
+
+            }
+        });
+
+
+
 
         TextView dateView=findViewById(R.id.textview_date);
-        TextView timeview=findViewById(R.id.textview_time);
+        //TextView timeview=findViewById(R.id.textview_time);
 
 
 
@@ -37,39 +74,52 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        RecyclerView homeRecyclerView=findViewById(R.id.home_recycler_view);
+        //RecyclerView homeRecyclerView=findViewById(R.id.home_recycler_view);
 
 
 
-        Task[] taskDetails={
+       /* Task[] taskDetails={
                 new Task("Class","OS class","11.00AM"),
                 new Task("Class","MPAL class","9.00AM"),
                 new Task("Class","SIM class","1.00pM"),
                 new Task("Class","MATH class","2.00pM"),
                 new Task("Class","NET class","10.00AM")
-        };
+        };*/
 
-        TaskAdapter taskAdapter=new TaskAdapter(taskDetails);
-        homeRecyclerView.setAdapter(taskAdapter);
+       // TaskAdapter taskAdapter=new TaskAdapter(taskDetails);
+       // homeRecyclerView.setAdapter(taskAdapter);
 
         //showDateTime(dateView);
         Calendar calender=Calendar.getInstance();
         SimpleDateFormat simpleDateFormat1=new SimpleDateFormat("EEEE, dd-MM-yyyy");
-        SimpleDateFormat simpleDateFormat2=new SimpleDateFormat("hh:mm a");
+       // SimpleDateFormat simpleDateFormat2=new SimpleDateFormat("hh:mm a");
 
         String date=simpleDateFormat1.format(calender.getTime());
-        String time=simpleDateFormat2.format(calender.getTime());
+       // String time=simpleDateFormat2.format(calender.getTime());
 
 
 
         dateView.setText(date);
-        timeview.setText(time);
+       // timeview.setText(time);
 
 
 
 
 
 
+    }
+
+    public void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.recyclerview_holder, fragment)
+                .commit();
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.recyclerview_holder,fragment);
+        fragmentTransaction.commit();
     }
 
 
